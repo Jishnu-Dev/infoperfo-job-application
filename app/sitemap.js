491ff00e13b07e077824ceea0504557a17f64940
Jishnu Raj <71690306+Jishnu-Dev@users.noreply.github.com>
2024-01-19 17:49:39 +0400

    Cleanup

--- app/sitemap.js
+++ app/sitemap.js
@@ -37,7 +37,7 @@ export default async function sitemap() {
 
   const sitemapEntries = [
     {
-      url: "/",
+      url: siteBasePath,
       lastModified: currentDate,
       changeFrequency: "daily",
       priority: 1,
